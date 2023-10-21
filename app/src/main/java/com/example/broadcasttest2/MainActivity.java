ee1bc65f764adc03acc931b0563c7ae0324c5464
wenyiYin <443644234@qq.com>
2023-10-21 15:58:45 +0800

    1.静态注册广播接收器，用于接受BroadcastTest项目发送的广播

--- app/src/main/java/com/example/broadcasttest2/MainActivity.java
+++ app/src/main/java/com/example/broadcasttest2/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.broadcasttest2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.IntentFilter;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,4 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
