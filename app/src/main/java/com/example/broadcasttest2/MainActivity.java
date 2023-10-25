6cf56bba03bc9131b6ec8b4eff4646447f586836
wenyiYin <443644234@qq.com>
2023-10-25 14:37:49 +0800

    2.动态注册receiverTest2广播，用于接受BroadcastTest项目发送的广播，并设置高优先级，在接收器中做拦截动作

--- app/src/main/java/com/example/broadcasttest2/MainActivity.java
+++ app/src/main/java/com/example/broadcasttest2/MainActivity.java
@@ -2,15 +2,42 @@ package com.example.broadcasttest2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private IntentFilter intentFilterTest2;
+    private Receiver receiverTest2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        intentFilterTest2 = new IntentFilter();
+        intentFilterTest2.addAction("com.example.MyBroadcast");
+        intentFilterTest2.setPriority(100);
+        receiverTest2 = new Receiver();
+        registerReceiver(receiverTest2, intentFilterTest2);
+    }
+
+    class Receiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(context, "receiverTest2：" + context.toString(), Toast.LENGTH_SHORT).show();
+            abortBroadcast();
+        }
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        unregisterReceiver(receiverTest2);
     }
 
 
